af888e5b9ddcea725b05ab1c7e5fc2fc3a97ba06
Mitar Djakovic <roker.93@hotmail.com>
2020-09-19 00:50:53 +0200

    Created common, development and production webpack

---
+++ webpack.config.common.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      inject: true,
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ]
+}
