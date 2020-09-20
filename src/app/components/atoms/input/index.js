df56d60c034b95d31845e78f248483dbc93c514b
Mitar Djakovic <roker.93@hotmail.com>
2020-09-20 03:08:45 +0200

    Prop type is no longer required, added formik for forms, created simple login form

--- src/app/components/atoms/input/index.js
+++ src/app/components/atoms/input/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import Input from './style';
 
-export default () => (
-  <Input placeholder="Text" />
+export default ({
+  value, name, type, onChange, id, placeholder,
+}) => (
+  <Input
+    placeholder={placeholder}
+    onChange={onChange}
+    type={type}
+    name={name}
+    id={id}
+    value={value}
+  />
 );
---
+++ src/app/components/organisms/loginForm/style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const FormContainer = styled.div`
+`;
+
+export {
+  FormContainer,
+};
