160599bd0bd1b37c44d5ca95788e2a67d725f1df
Mitar <Mitar Djakovic>
2020-11-04 15:38:02 +0100

    Added side nav

--- src/app/views/home/index.js
+++ src/app/views/home/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import TopNav from '../../components/molecules/topNav';
+import SideNav from '../../components/molecules/sideNav';
 
 const HomeView = () => (
-  <div style={{ backgroundColor: '#f4f7fc', width: '100%', height: '100vh' }}>
+  <div style={{
+    backgroundColor: '#f4f7fc', width: '100%', height: '100vh', position: 'relative',
+  }}
+  >
     <TopNav />
+    <SideNav />
   </div>
 );
 
